3a808c2588dfa184498116ceb4cd81a5d52fb5b0
guillaumemillot <guillaume.millot@gmail.com>
2014-12-28 16:39:46 +0100

    Test 4

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -18,8 +18,6 @@ import java.util.List;
 
 public class MainActivity extends ActionBarActivity {
 
-    // Test 3
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
